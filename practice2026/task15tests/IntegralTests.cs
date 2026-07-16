101ddd2a6cd0f5a766a3e72149659b4063efc78c
Dmitrij Rac <dmitrijrac67@gmail.com>
2026-07-17 04:22:10 +0600

    task15 with results

---
+++ practice2026/task15tests/IntegralTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xunit;
+using task14;
+
+namespace task14tests;
+
+public class IntegralTests
+{
+    [Fact]
+    public void CheckLinearFunction_Symmetric_IsZero()
+    {
+        Func<double, double> func = x => x;
+        Assert.Equal(0, DefiniteIntegral.Solve(-1, 1, func, 1e-4, 2), 1e-4);
+    }
+
+    [Fact]
+    public void CheckSinFunction_Symmetric_IsZero()
+    {
+        Func<double, double> func = Math.Sin;
+        Assert.Equal(0, DefiniteIntegral.Solve(-1, 1, func, 1e-5, 8), 1e-4);
+    }
+
+    [Fact]
+    public void CheckLinearFunction_Positive_IsCorrect()
+    {
+        Func<double, double> func = x => x;
+        Assert.Equal(12.5, DefiniteIntegral.Solve(0, 5, func, 1e-6, 8), 1e-5);
+    }
+}
